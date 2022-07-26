6372582e31c023decc920f705e2434d40f86d83c
Gaz <dianghazy@protonmail.com>
2022-07-26 15:45:19 +0700

    Gaz: endpoints for prior & likelihoods

--- apis/likely.py
+++ apis/likely.py
@@ -1,19 +1,25 @@
 from typing import List
 from flask_restx import Namespace, Resource
 from flask_cors import cross_origin
-from database.query import readLoglikelihood
+from database.query import readLoglikelihood, readLogprior
 from .wrapper import secure
 from typing import List
 
 api = Namespace(
-    'likelihoods',
-    description='daftar kata dan nilai likelihoods-nya',
+    'logs',
+    description='nilai prior dan daftar nilai likelihood setiap kata',
     decorators=[cross_origin()]
 )
 
-@api.route('/')
+@api.route('/likelihoods/')
 class readLikelihood(Resource):
     method_decorators = [secure]
     def get(self) -> List:
         lh = readLoglikelihood()
         return [{"kata": lh[x], "likelihood": x} for x in lh]
+
+@api.route('/prior')
+class readPrior(Resource):
+    method_decorators = [secure]
+    def get(self) -> float:
+        return readLogprior()
